6d21cb8d7ba5061f37909f5c2762ce4231f4c7bf
jose <joaerazogo@unal.edu.co>
2019-02-18 13:06:26 -0500

    sección de angular finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ClienteService } from './clientes/cliente.service'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/clientes', pathMatch: 'full'},
+  {path: 'directivas', component: DirectivaComponent},
+  {path: 'clientes', component: ClientesComponent}
+];
 
 @NgModule({
   declarations: [
@@ -18,9 +25,10 @@ import { ClientesComponent } from './clientes/clientes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,15 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'Andrés', apellido: 'Gómez', email: 'andresGomez@gmail.com', createAt: '2017-12-11'},
+  {id: 2, nombre: 'Mr. John', apellido: 'Doe', email: 'john.doe@gmail.com', createAt: '2017-12-12'},
+  {id: 3, nombre: 'Linus', apellido: 'Torvlds', email: 'linus.torvalds@gmail.com', createAt: '2017-12-13'},
+  {id: 4, nombre: 'Rasmus', apellido: 'Lerdorf', email: 'rasmus.lerdor@gmail.com', createAt: '2017-12-14'},
+  {id: 5, nombre: 'Erich', apellido: 'Gamma', email: 'erich.gama@gmail.com', createAt: '2017-12-15'},
+  {id: 6, nombre: 'Richard', apellido: 'Helm', email: 'richard.helm@gmail.com', createAt: '2017-12-16'},
+  {id: 7, nombre: 'Ralph', apellido: 'Johnson', email: 'ralph.johnson@gmail.com', createAt: '2017-12-17'},
+  {id: 8, nombre: 'Bombasto', apellido: 'Vlissides', email: 'bombasto.vlissides@gmail.com', createAt: '2017-12-18'},
+  {id: 9, nombre: 'Dr James', apellido: 'Gosling', email: 'james.gosling@gmail.com', createAt: '2017-12-19'},
+  {id: 10, nombre: 'Magma', apellido: 'Lee', email: 'magma.lee@gmail.com', createAt: '2017-12-20'},
+  {id: 11, nombre: 'Tornado', apellido: 'Roe', email: 'tornado.roe@gmail.com', createAt: '2017-12-21'}
+]
