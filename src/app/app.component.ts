6f97e6145a12a260ce416aadb83eb8e7fc5e30ed
Jose Augusto Erazo Gomez <jerazo@cidenet.com.co>
2019-04-24 16:31:50 -0500

    se eliminan algunos archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'the jungle';
+  title = 'the Application';
   curso: string = 'Curso Spring 5 con angular';
   profesor: string ='maestro';
 }
